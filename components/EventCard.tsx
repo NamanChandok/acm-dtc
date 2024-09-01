bbeeb3c2a9bcce781f35e5df0276618eecbfee6a
Naman Chandok <namanchandok1@gmail.com>
2024-09-02 03:05:42 +0530

    fix event links home page

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { ArrowOutward } from '@mui/icons-material'
 
 type Props = {
+    id: string,
     name: string,
     date: string,
     cover: string,
     link: string
 }
 
-export default function EventCard({name, date, cover, link}: Props) {
+export default function EventCard({id, name, date, cover, link}: Props) {
   return (
     <div className='rounded-lg w-80 flex-shrink-0 shadow-md overflow-hidden'>
         <img src={cover} alt={name} className='aspect-square w-full bg-[#eee] object-cover' />
@@ -17,7 +18,7 @@ export default function EventCard({name, date, cover, link}: Props) {
               <small className='text-[#eee] text-sm'>{date}</small>
               <h3 className='text-white text-xl truncate font-semibold'>{name}</h3>
             </div>
-            <a href={link} className='w-1/4 text-end'>
+            <a href={`/events/${id}`} className='w-1/4 text-end'>
               <ArrowOutward className="bg-black text-white h-14 w-14 p-4 rounded-full hover:p-3 transition-all duration-300" />
             </a>
         </div>
