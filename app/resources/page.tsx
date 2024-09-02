79e7af9844d82a869b17029fac394326f645871e
Naman Chandok <namanchandok1@gmail.com>
2024-09-02 15:55:07 +0530

    change worker

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -12,7 +12,8 @@ import 'react-pdf/dist/Page/TextLayer.css';
 
 export default function Resources() {
 
-  pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;  
+  // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;  
 
   const [resources, setResources] = useState<string[]>([]);
   
