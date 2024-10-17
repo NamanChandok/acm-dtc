90a682f4d10f4687289168299fda38624686f9d0
Naman Chandok <namanchandok1@gmail.com>
2024-10-17 17:42:02 +0530

    team data

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -7,33 +7,7 @@ import { Quantico } from "next/font/google";
 
 const quantico = Quantico({subsets: ['latin'], weight: ['400', '700']});
 
-const team = [
-  {name: "Kartick Chauhaan", role: "Chairperson", image: "/team/kartick_c.jpg", year: "4th Year, B.Tech CSE", linkedin: "https://www.linkedin.com/in/kartick-chauhaan-123172236/", mail: "mailto:chauhaankartick@gmail.com"},
-  {name: "Ananya Mathur", role: "Vice Chairperson", image: "/team/ananya_m.jpeg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Kritagya Arora", role: "Webmaster", image: "/team/kritagya.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Kishika Sharma", role: "Secretary", image: "/team/kishika.png", year: "4th Year, B.Tech AIML", linkedin: "/", mail: "/"},
-  {name: "Ananya Tamta", role: "Secretary Associate", image: "/team/ananya_t.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Danish", role: "Treasurer", image: "/team/danish.jpg", year: "3rd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Aashi", role: "Membership Chair", image: "/team/aashi.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Akshit Lakhanpal", role: "PR Outreach Head", image: "/team/akshit.jpeg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Harshit Panchal", role: "PR Outreach Associate", image: "/team/harshit.jpeg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Harshita Meena", role: "PR Outreach Associate", image: "/team/harshita.png", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Adityan Verma", role: "Operation Head", image: "/team/adityan.jpg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Vansh Jaiswal", role: "Discipline Head", image: "/team/vansh.jpg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Isha Vashishtha", role: "Discipline Associate", image: "/team/isha.jpg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Aakash Chopra", role: "Discipline Associate", image: "/team/aakash.jpg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Mohd. Akif", role: "Discipline Associate", image: "/team/akif.jpg", year: "3rd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Prateek Srivastav", role: "Design Head", image: "/team/prateek.jpeg", year: "4th Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Shah Kamil Ahmad", role: "Design Associate", image: "/team/kamil.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Shubham Katyan", role: "Social Head", image: "/team/shubham.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Kovidh Nougain", role: "Social Associate", image: "/team/kovidh.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Sanjam Kaur", role: "Social Associate", image: "/team/sanjam.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Naman Chandok", role: "Web Associate", image: "/team/naman.jpeg", year: "3rd Year, B.Tech CSE", linkedin: "https://www.linkedin.com/in/namanchandok/", mail: "mailto:namanchandok1@gmail.com"},
-  {name: "Yuvraj Singh", role: "Web Associate", image: "/team/yuvraj.jpeg", year: "3rd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Abhinav Chauhaan", role: "Videographer", image: "/team/abhinav.jpg", year: "2nd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Brahmjyot Singh", role: "Videographer", image: "/team/brahmjyot.jpg", year: "3rd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-  {name: "Kartick Sharma", role: "Videographer", image: "/team/kartick_s.jpg", year: "3rd Year, B.Tech CSE", linkedin: "/", mail: "/"},
-]
+import team from "./team_data";
 
 export default function Team() {
 
---
+++ app/team/team_data.ts
@@ -0,0 +1,180 @@
+const team = [
+    {
+        name: "Kartick Chauhaan",
+        role: "Chairperson",
+        image: "/team/kartick_c.jpg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/kartick-chauhaan-123172236/",
+        mail: "mailto:chauhaankartick@gmail.com"
+    }, {
+        name: "Ananya Mathur",
+        role: "Vice Chairperson",
+        image: "/team/ananya_m.jpeg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/ananya-mathur-a88a77249",
+        mail: "mailto:ananyamathurbtech@delhitechnicalcampus.ac.in"
+    }, {
+        name: "Kritagya Arora",
+        role: "Webmaster",
+        image: "/team/kritagya.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/kritagya-arora-659b23193",
+        mail: "mailto:arora.kritagyaa@gmail.com"
+    }, {
+        name: "Kishika Sharma",
+        role: "Secretary",
+        image: "/team/kishika.png",
+        year: "4th Year, B.Tech AIML",
+        linkedin: "https://www.linkedin.com/in/kishikasharma",
+        mail: "mailto:kishikasharmabtech@delhitechnicalcampus.ac.in"
+    }, {
+        name: "Ananya Tamta",
+        role: "Secretary Associate",
+        image: "/team/ananya_t.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/ananya-tamta-1392b12a3",
+        mail: "mailto:ananyatamta26@gmail.com"
+    }, {
+        name: "Danish",
+        role: "Treasurer",
+        image: "/team/danish.jpg",
+        year: "3rd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/mddanish4338",
+        mail: "mailto:danish786.aps@gmail.com"
+    }, {
+        name: "Aashi",
+        role: "Membership Chair",
+        image: "/team/aashi.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/aashi2926",
+        mail: "mailto:aashi_a44@delhitechnicalcampus.ac.in"
+    }, {
+        name: "Akshit Lakhanpal",
+        role: "PR Outreach Head",
+        image: "/team/akshit.jpeg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "/"
+    }, {
+        name: "Harshit Panchal",
+        role: "PR Outreach Associate",
+        image: "/team/harshit.jpeg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/harshit-panchal-a593b9176",
+        mail: "mailto:panchalharshit089@gmail.com"
+    }, {
+        name: "Harshita Meena",
+        role: "PR Outreach Associate",
+        image: "/team/harshita.png",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/harshita-meena-a7591a26a",
+        mail: "mailto:mharshita087@gmail.com"
+    }, {
+        name: "Adityan Verma",
+        role: "Operation Head",
+        image: "/team/adityan.jpg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "/"
+    }, {
+        name: "Vansh Jaiswal",
+        role: "Discipline Head",
+        image: "/team/vansh.jpg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/vansh-jaiswal-a46768287",
+        mail: "mailto:vanshhjaiswall@gmail.com"
+    }, {
+        name: "Isha Vashishtha",
+        role: "Discipline Associate",
+        image: "/team/isha.jpg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "/"
+    }, {
+        name: "Aakash Chopra",
+        role: "Discipline Associate",
+        image: "/team/aakash.jpg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "/"
+    }, {
+        name: "Mohd. Akif",
+        role: "Discipline Associate",
+        image: "/team/akif.jpg",
+        year: "3rd Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "/"
+    }, {
+        name: "Prateek Srivastav",
+        role: "Design Head",
+        image: "/team/prateek.jpeg",
+        year: "4th Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/prateek-srivastava-600bb9262",
+        mail: "mailto:prateeksrivastav981@gmail.com"
+    }, {
+        name: "Shah Kamil Ahmad",
+        role: "Design Associate",
+        image: "/team/kamil.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/kamil-ahmad-824707237",
+        mail: "mailto:kamil_b03@delhitechnicalcampus.ac.in"
+    }, {
+        name: "Shubham Katyan",
+        role: "Social Head",
+        image: "/team/shubham.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/shubham-katyan-378b051b9",
+        mail: "mailto:shubhamkatyan64@gmail.com"
+    }, {
+        name: "Kovidh Nougain",
+        role: "Social Associate",
+        image: "/team/kovidh.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/kovidh-nougain-bab68a1b0",
+        mail: "mailto:kovidh4444@gmail.com"
+    }, {
+        name: "Sanjam Kaur",
+        role: "Social Associate",
+        image: "/team/sanjam.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/sanjammkaurr",
+        mail: "mailto:sanjamkaur23@gmail.com"
+    }, {
+        name: "Naman Chandok",
+        role: "Web Associate",
+        image: "/team/naman.jpeg",
+        year: "3rd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/namanchandok/",
+        mail: "mailto:namanchandok1@gmail.com"
+    }, {
+        name: "Yuvraj Singh",
+        role: "Web Associate",
+        image: "/team/yuvraj.jpeg",
+        year: "3rd Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "/"
+    }, {
+        name: "Abhinav Chauhaan",
+        role: "Videographer",
+        image: "/team/abhinav.jpg",
+        year: "2nd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/abhinavaryan",
+        mail: "mailto:abhinav_c11@delhitechnicalcampus.ac.in"
+    }, {
+        name: "Brahmjyot Singh",
+        role: "Videographer",
+        image: "/team/brahmjyot.jpg",
+        year: "3rd Year, B.Tech CSE",
+        linkedin: "https://www.linkedin.com/in/brahmjyot",
+        mail: "mailto:brahmjyots@icloud.com"
+    }, {
+        name: "Kartick Sharma",
+        role: "Videographer",
+        image: "/team/kartick_s.jpg",
+        year: "3rd Year, B.Tech CSE",
+        linkedin: "/",
+        mail: "mailto:"
+    },
+]
+
+export default team;
